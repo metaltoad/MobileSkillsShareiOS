83d23550ee445d43ad27177c0a2f641004468abf
Phil Tseng <phil.tseng@metaltoad.com>
2017-11-13 13:15:23 -0800

    Add Challenge 1

---
+++ Challenge1/Challenge1/AppDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.swift
+//  Challenge1
+//
+//  Created by Phil Tseng on 11/13/17.
+//  Copyright © 2017 Metal Toad. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+  
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
---
+++ Challenge1/Challenge1/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Challenge1
+//
+//  Created by Phil Tseng on 11/13/17.
+//  Copyright © 2017 Metal Toad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
